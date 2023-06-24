dc7e07c0a96af35fa1cb92cdac5e8dc774209f80
Andrii <sushkoandy1@gmail.com>
2023-06-24 16:53:14 +0300

    add currency exchange

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,1 +1,2 @@
 export const selectBaseCurrency = state => state.currency.baseCurrency;
+export const selectExchanged = state => state.currency.result;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,33 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { currencyReducer } from './slice';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, currencyReducer);
 export const store = configureStore({
   reducer: {
-    currency: currencyReducer,
+    currency: persistedReducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
